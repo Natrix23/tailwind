95a3e8b43e12a2692809c9ebfb84a9729412619e
Natrix23 <43995175+Natrix23@users.noreply.github.com>
2024-12-19 00:29:07 +0100

    Add files via upload

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter var", "ui-sans-serif"],
+      system: ["system-ui"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
